2abf0bd0137018446d00aacdb769d4d3a2775056
Motaz HM <motazhajm@gmail.com>
2024-07-08 10:33:33 +0300

    Implement adding bug form interface

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
+import DefaultContainer from "./components/layout/DefaultContainer";
+
 function App() {
   return (
     <>
-      <h1>Bug Tracker</h1>
+      <DefaultContainer>
+        <h1 className="text-2xl font-bold text-center mt-12">My Bug Tracker</h1>
+
+        <form className="flex h-12 rounded-xl overflow-hidden">
+          <input
+            className="w-full h-full px-4 text-black"
+            name="newbug"
+            id="newbug"
+            type="text"
+            placeholder="Add a new bug"
+          />
+          <button className="bg-[#F29F05] hover:bg-[#e6b968] w-32 h-full px-4" type="submit">
+            Add Bug
+          </button>
+        </form>
+      </DefaultContainer>
     </>
   );
 }
