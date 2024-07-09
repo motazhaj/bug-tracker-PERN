e9914c092cd14c33d3995ab18828c4568c4952b7
Motaz HM <motazhajm@gmail.com>
2024-07-09 17:42:37 +0300

    Refactor bug listing component

--- client/src/components/ListBugs.jsx
+++ client/src/components/ListBugs.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
+import BugDetails from "./BugDetails";
+import Resolved from "./Resolved";
 
 const ListBugs = () => {
   const [bugs, setBugs] = useState([]);
@@ -23,18 +25,24 @@ const ListBugs = () => {
           <tr>
             <th className="text-start">Name</th>
             <th className="text-start">Date</th>
-            <th className="text-start">Edit</th>
-            <th className="text-start">Delete</th>
+            <th className="text-start">Status</th>
+            <th className="text-start">Activity</th>
+            <th className="text-end">Actions</th>
           </tr>
         </thead>
 
         {bugs.map((bug) => (
-          <tbody key={bug.bug_id}>
-            <tr className="bg-white/30 border-y border-white/30">
+          <tbody key={bug.bug_id} className="border-y border-white/30">
+            <tr>
               <td>{bug.title}</td>
               <td>{dayjs(bug.creationdate).format("h:mm A - DD MMM YYYY")}</td>
-              <td className="text-[#f2f2f2] hover:text-[#fff1c2] cursor-pointer">Edit</td>
-              <td className="text-[#f2f2f2] hover:text-[#ffa6a6] cursor-pointer">Delete</td>
+              <td>
+                <Resolved resolved={bug.resolved} setBugs={setBugs} />
+              </td>
+              <td>{bug.updatedate ? dayjs(bug.updatedate).format("h:mm A - DD MMM YYYY") : "N/A"}</td>
+              <td className="text-end">
+                <BugDetails id={bug.bug_id} />
+              </td>
             </tr>
           </tbody>
         ))}
