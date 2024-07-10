3b325bec44385bb992d18508ebff59456de6ee8f
Motaz HM <motazhajm@gmail.com>
2024-07-10 16:56:45 +0300

    Add resolving bug API

--- server/index.js
+++ server/index.js
@@ -39,6 +39,7 @@ app.post("/newbug", async (req, res) => {
       title,
       description,
     ]);
+    console.log(newBug.rows[0]);
     res.json(newBug.rows[0]);
   } catch (err) {
     console.error(err.message);
@@ -49,12 +50,28 @@ app.put("/bugs/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { title, description, resolved } = req.body;
-
+    console.log(req.body);
     const updateBug = await pool.query(
       "UPDATE bugs SET title = COALESCE($2, title),description = COALESCE($3, description), resolved = COALESCE($4, resolved), updateDate = current_timestamp WHERE bug_id = $1 RETURNING *",
       [id, title, description, resolved]
     );
 
+    res.json(updateBug.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.put("/resolvebug/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { resolved } = req.body;
+
+    const updateBug = await pool.query(
+      "UPDATE bugs SET resolved = COALESCE($2, resolved), updateDate = current_timestamp WHERE bug_id = $1 RETURNING *",
+      [id, resolved]
+    );
+    console.log(req.body);
     res.json(updateBug.rows[0]);
   } catch (err) {
     console.error(err.message);
