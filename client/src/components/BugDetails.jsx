79b0cfff869adb0d7618335cc237fd0a71437aab
Motaz HM <motazhajm@gmail.com>
2024-07-10 13:28:07 +0300

    Refactor bug details component

--- client/src/components/BugDetails.jsx
+++ client/src/components/BugDetails.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { IoIosMore } from "react-icons/io";
+import { FaRegTrashAlt } from "react-icons/fa";
 import Resolved from "./Resolved";
 
-export default function BugDetails({ id, setBugs }) {
+export default function BugDetails({ id, bugs, setBugs }) {
   const [showBugDetails, setShowBugDetails] = React.useState(false);
   const [bug, setBug] = useState({});
 
@@ -25,24 +28,44 @@ export default function BugDetails({ id, setBugs }) {
 
       setBugs(bugs.filter((bug) => bug.bug_id !== id));
     } catch (err) {
-      console.error(err);
+      console.error(err.message);
     }
   };
 
   return (
     <>
-      <button className="w-20" type="button" onClick={() => setShowBugDetails(true)}>
-        Open
+      <button className="w-10 hover:scale-125 duration-200" type="button" onClick={() => setShowBugDetails(true)}>
+        <IoIosMore size={24} />
       </button>
       {showBugDetails ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-4xl bg-white/10 px-8 pt-4 rounded-sm">
-              <form className="border-0 rounded-lg shadow-lg relative flex flex-col w-full  outline-none focus:outline-none">
-                <div className="text-start py-8 flex flex-col gap-4">
-                  <input type="text" value={bug.title} className="block w-full bg-transparent border-none" />
-                  <input type="text" value={bug.description} className="block w-full bg-transparent border-none" />
-                  <Resolved resolved={bug.resolved} />
+            <div className="relative my-6 mx-auto w-[1280px] h-[600px] bg-white/10 px-8 pt-4 rounded-md">
+              <form className="border-0 rounded-lg h-full relative flex flex-col justify-between w-full  outline-none focus:outline-none">
+                <button className="hover:scale-125 duration-200 ml-auto" type="button" onClick={() => deleteBug(id)}>
+                  <FaRegTrashAlt size={24} />
+                </button>
+                <div className="text-start h-full pb-8 flex flex-col justify-between py-8">
+                  <TextareaAutosize
+                    type="text"
+                    minRows={2}
+                    value={bug.title}
+                    className="block w-full bg-transparent border-none"
+                  />
+                  <TextareaAutosize
+                    type="text"
+                    minRows={2}
+                    placeholder="Add description"
+                    value={bug.description}
+                    className="block w-full bg-transparent border-none"
+                  />
+                  <Resolved
+                    resolved={bug.resolved}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setBug({ ...bug, resolved: !bug.resolved });
+                    }}
+                  />
                 </div>
 
                 <div className="flex items-center justify-end p-6 border-t border-white/30 border-solid border-blueGray-200 rounded-b">
